f8aded2a69a98685c4f4e937fa2ae442425c9c64
Patrick Burris <nchupacabra@gmail.com>
2019-04-02 12:49:24 -0500

    add support for database connection on boot

--- boot.sh
+++ boot.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 source venv/bin/activate
+
+if [ -z "${DATABASE_URL}" ]; then
+  echo "No attached database"
+else
+  while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Upgrade command failed, retrying in 5 secs...
+    sleep 5
+  done
+fi
+
 exec gunicorn entry:app --bind '0.0.0.0:5000' --access-logfile - --error-logfile -
