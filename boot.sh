6c6aa3c02b0966292779ccbd41437ff2353ccbde
Patrick Burris <nchupacabra@gmail.com>
2019-04-02 14:11:48 -0500

    add docker-compose local dev support

--- boot.sh
+++ boot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source venv/bin/activate
 
+# If there is an attached DB, wait until we can connect to it and run an upgrade
 if [ -z "${DATABASE_URL}" ]; then
   echo "No attached database"
 else
@@ -14,4 +15,10 @@ else
   done
 fi
 
-exec gunicorn entry:app --bind '0.0.0.0:5000' --access-logfile - --error-logfile -
+# If we are in debug mode, run the development server
+if [ $FLASK_DEBUG == 1 ]; then
+  exec flask run --host=0.0.0.0 --port=5000
+else
+  exec gunicorn entry:app --bind '0.0.0.0:5000' --access-logfile - --error-logfile -
+fi
+
